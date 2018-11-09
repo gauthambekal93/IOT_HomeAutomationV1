580e46dc51620bbf3260405473c698ff7779ff02
gautham <gauthambekal93@gmail.com>
2018-11-08 21:22:32 -0500

    commit4

---
+++ app/src/main/java/com/example/gauth/iot_homeautomationv1/Constants.java
@@ -0,0 +1,7 @@
+package com.example.gauth.iot_homeautomationv1;
+
+public class Constants {
+    public static String ROOT_URL = "http://10.216.55.46/Android/v1/";
+    public static String URL_REGISTER =ROOT_URL+"registerUser.php";
+    public static String URL_LOGIN =ROOT_URL+"userLogin.php";
+}
